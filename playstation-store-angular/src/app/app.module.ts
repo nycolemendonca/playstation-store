409fcb9bb011cc5c6859d19e2a10d927575d797c
nycolemendonca <nycolemendonca4@gmail.com>
2023-06-21 09:18:36 -0300

    add user interface

---
+++ playstation-store-angular/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { CardComponent } from './components/card/card.component';
+import { CardLabelComponent } from './components/card/card-label/card-label.component';
+import { CardPricingComponent } from './components/card/card-pricing/card-pricing.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    MenuBarComponent,
+    CardComponent,
+    CardLabelComponent,
+    CardPricingComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ playstation-store-angular/src/app/components/card/card-pricing/card-pricing.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pricing',
+  templateUrl: './card-pricing.component.html',
+  styleUrls: ['./card-pricing.component.css']
+})
+export class CardPricingComponent {
+  @Input() cardPricing: String = "";
+  @Input() cardDevice: String = "";
+}
